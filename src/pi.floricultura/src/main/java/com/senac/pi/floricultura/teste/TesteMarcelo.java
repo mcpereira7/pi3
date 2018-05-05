6b8a39f94742cac70e056d9f8e94808746bf39c5
Marcelo Pereira <macope727@gmail.com>
2018-05-05 17:55:45 -0300

    Acertando Arquivos de estilo e scripts

--- src/pi.floricultura/src/main/java/com/senac/pi/floricultura/teste/TesteMarcelo.java
+++ src/pi.floricultura/src/main/java/com/senac/pi/floricultura/teste/TesteMarcelo.java
@@ -7,6 +7,7 @@ package com.senac.pi.floricultura.teste;
 
 import com.senac.pi.floricultura.DAO.EnderecoDAO;
 import com.senac.pi.floricultura.controllers.ServicoCliente;
+import com.senac.pi.floricultura.model.Cliente;
 import com.senac.pi.floricultura.model.Endereco;
 import com.senac.pi.floricultura.model.GerarCodigo;
 import com.senac.pi.floricultura.model.PessoaFisica;
@@ -30,13 +31,14 @@ public class TesteMarcelo {
         String date = formato.format(data);
         Date dtCad = formato.parse(date);
         Date dtNasc = formato.parse("2000-04-15");
-        PessoaFisica cliente = new PessoaFisica("77777777777", 1, dtNasc, "bbbbb@bbb.bb", "1111111111", "11222222222", codObjeto, "GGGGG BBBB kkkkkkkk", 10, dtCad, false);
+        PessoaFisica cliente = new PessoaFisica("88888888888", 1, dtNasc, "qqqqqq@kkk.rs", "1111111111", "11222222222", codObjeto, "Nenhum Alguém de Seilá", 10, dtCad, false);
         PessoaJuridica clientePJ = new PessoaJuridica("11555666999273", "111111111111", true, false, "Responsável", "resp@aaaa.com", "1122223333", "1122224444", codObjeto, "Empresa BBBBB", "BBBBB Empresa", 2, data, false);
         Endereco endereco = new Endereco(cliente.getId(),"99999999", "Av de Teste EEEEEEEEEE", "5", "ap 10", "CCCCCCC", "Cidade MMMMMMM", "PP");
         Endereco enderecopj = new Endereco(clientePJ.getId(),"04718000", "Rua Capitão Otávio Machado", "492", "", "Chac Sto Antonio", "Cidade São Paulo", "SP");
+        Cliente cliPf = new Cliente(cliente, endereco);
         try {
-            ServicoCliente.cadastrarClientePF(cliente);
-            EnderecoDAO.inserirEndereco(endereco, cliente.getId());
+            ServicoCliente.cadastrarClientePF(cliPf.getCliPf());
+            EnderecoDAO.inserirEndereco(cliPf.getEndereco(), cliente.getId());
             
         } catch (Exception e) {
             System.out.println("Achou mesmo que seria assim tão fácil?");
