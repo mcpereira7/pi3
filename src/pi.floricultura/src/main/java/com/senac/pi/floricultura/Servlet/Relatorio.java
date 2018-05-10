6cd1a3d41c09ddeaac5aa968b2a6cc899ba15164
Vinicius Presoto <vini_presoto@hotmail.com>
2018-05-09 21:39:30 -0300

    Alterado modo de recuperar relatorio - Datas formatadas

--- src/pi.floricultura/src/main/java/com/senac/pi/floricultura/Servlet/Relatorio.java
+++ src/pi.floricultura/src/main/java/com/senac/pi/floricultura/Servlet/Relatorio.java
@@ -6,10 +6,16 @@
 package com.senac.pi.floricultura.Servlet;
 
 import com.senac.pi.floricultura.controllers.ServicoVenda;
+import com.senac.pi.floricultura.exceptions.VendaException;
 import com.senac.pi.floricultura.model.Venda;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,13 +32,27 @@ public class Relatorio extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        List<Venda> venda = ServicoVenda.ConsultaVendaByData(request.getParameter("dataInicial"), request.getParameter("dataFinal"));
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dtIni = null;
+        Date dtFim = null;
+        List<Venda> venda = null;
+        try {
+            dtIni = dtFormat.parse(request.getParameter("dataInicial"));
+            dtFim = dtFormat.parse(request.getParameter("dataFinal"));
+        } catch (ParseException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+             venda = ServicoVenda.ConsultaVendaByData(dtIni, dtFim);
+        } catch (VendaException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
         request.getRequestDispatcher("WEB-INF/jsp/Relatorio.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 }
